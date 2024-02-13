a0d9b41d30259503a4e2aeb0eab3a0dc0d91d3ab
Russell <minghao9239@126.com>
2024-02-13 15:28:00 +0800

    ti

--- little/3.requests_params.py
+++ little/3.requests_params.py
@@ -13,7 +13,7 @@ headers = {
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0'
 }
 data = {
-    'wd' : 'python'
+    'wd' : 'baidu'
 }
 response = requests.get(url , headers = headers , params = data)
 with open('baidu1.html' , 'wb')as f:
